24c94bf73645c3cca5d1bdf70d4a3349559bbe85
Isha013 <idavda079@rku.ac.in>
2024-12-24 11:38:26 +0530

    add sumoftwonum

---
+++ WebApplication1/sumoftwonum.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="sumoftwonum.aspx.cs" Inherits="WebApplication1.sumoftwonum" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Number1"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Number2"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <p>
+            <asp:Label ID="LableResult" runat="server" Text="Result"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/sumoftwonum.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class sumoftwonum : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Retrieve the numbers entered in the textboxes
+                int number1 = int.Parse(TextBox1.Text);
+                int number2 = int.Parse(TextBox2.Text);
+
+                // Calculate the sum
+                int sum = number1 + number2;
+
+                // Display the result
+                LableResult.Text = "The sum is: " + sum;
+            }
+            catch (Exception ex)
+            {
+                // Handle any potential exceptions
+                LableResult.Text = "Error: " + ex.Message;
+            }
+        }
+    }
+}
