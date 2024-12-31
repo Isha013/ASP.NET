1d02c567848ca0558ce4d23c92f4aac35ce8ba31
Isha013 <idavda079@rku.ac.in>
2024-12-31 11:29:11 +0530

    create AdRotator

---
+++ WebApplication1/WebForm2.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="WebApplication1.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:AdRotator ID="AdRotator1" runat="server" AdvertisementFile="~/add.xml" Height="400px" Width="400px" />
+        <p>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+        </p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm2.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string fileName = FileUpload1.FileName;
+                FileUpload1.SaveAs(Server.MapPath("~/Uploads/") + fileName);
+                Label1.Text = "File uploaded successfully!";
+            }
+            else
+            {
+                Label1.Text = "Please select a file to upload!";
+            }
+        }
+    }
+}
